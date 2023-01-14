57068339fa14a5018e6c35424604dcbbf060a259
Jan Liby Dela Costa <janlibydelacosta@gmail.com>
2023-01-14 12:35:21 +0800

    Update app.js

--- app.js
+++ app.js
@@ -27,6 +27,20 @@ if(message == "Hello") {
 if(message == "Hi") {
 	api.sendMessage("Hello", event.threadID);
 }
+
+if(startsWith("Kei")) {
+    const axios = require("axios");
+    let text = message.substring(5);
+    let data = message.split(" ");
+    if (data.length < 2) {
+    	api.sendMessage(`⚠️Invalid Use Of Command!\n💡Usage: ${prefix}kei [ask]`, event.threadID);
+    } else {
+        let a = axios.get(`https://api.libyzxy0.repl.co/api/aiAsk?query=${text}`);
+        a.then(response => {
+        	api.sendMessage(response.data.result.message, event.threadID, event.messageID)
+  })
+    }
+}
                
                
                
